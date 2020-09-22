4619c1df3acfe460aa1e2e6a8ae559a74b95301a
Casey McQuillan <Casey@quilltechnologies.com>
2020-09-22 14:30:41 -0700

    Adding basic short links and litedb.

--- url-shortener-tutorial/Program.cs
+++ url-shortener-tutorial/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(builder =>
@@ -21,13 +22,51 @@ var host = Host.CreateDefaultBuilder(args)
             // Use the endpoint routing module to map URLs to functions.
             app.UseEndpoints((endpoints) =>
             {
+                // Serve root index.html
                 endpoints.MapGet("/", (ctx) =>
                 {
                     return ctx.Response.SendFileAsync("index.html");
                 });
+
+                endpoints.MapPost("/shorten", HandleShortenUrl);
             });
         });
     })
     .Build();
 
 await host.RunAsync();
+
+// Endpoint Methods
+
+static Task HandleShortenUrl(HttpContext context)
+{
+    // Perform basic form validation
+    if (!context.Request.HasFormContentType || !context.Request.Form.ContainsKey("url"))
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return context.Response.WriteAsync("Cannot process request");
+    }
+
+    context.Request.Form.TryGetValue("url", out var formData);
+    var requestedUrl = formData.ToString();
+
+    // Test our URL
+    if (!Uri.TryCreate(requestedUrl, UriKind.Absolute, out Uri result))
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return context.Response.WriteAsync("Could not understand URL.");
+    }
+
+    var url = result.ToString();
+
+    // Temporary short link 
+    var entry = new ShortLink
+    {
+        Id = 123_456_789,
+        Url = url
+    };
+
+    var urlChunk = entry.GetUrlChunk();
+    var responseUri = $"{context.Request.Scheme}://{context.Request.Host}/{urlChunk}";
+    return context.Response.WriteAsync(responseUri);
+}
--- url-shortener-tutorial/ShortLink.cs
+++ url-shortener-tutorial/ShortLink.cs
@@ -5,11 +5,13 @@ public class ShortLink
 {
     public string GetUrlChunk()
     {
+        // Transform the "Id" property on this object into a short piece of text
         return WebEncoders.Base64UrlEncode(BitConverter.GetBytes(Id));
     }
 
     public static int GetId(string urlChunk)
     {
+        // Reverse our short url text back into an interger Id
         return BitConverter.ToInt32(WebEncoders.Base64UrlDecode(urlChunk));
     }
 
