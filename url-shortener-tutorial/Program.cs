5a6c8fced4d333a7319ea1b0dc205343fb37bcd5
Casey McQuillan <Casey@quilltechnologies.com>
2020-09-22 11:15:09 -0700

    Basic endpoint routing.

---
+++ url-shortener-tutorial/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(builder =>
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Add the necessary components for HTTP routing.
+            services.AddRouting();
+        })
+        .Configure(app =>
+        {
+            // Use routing in the pipeline.
+            app.UseRouting();
+
+            // Use the endpoint routing module to map URLs to functions.
+            app.UseEndpoints((endpoints) =>
+            {
+                endpoints.MapGet("/", (ctx) =>
+                {
+                    return ctx.Response.WriteAsync("Hello, World!");
+                });
+            });
+        });
+    })
+    .Build();
+
+await host.RunAsync();
